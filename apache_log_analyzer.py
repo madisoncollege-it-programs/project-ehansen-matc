1836306d460f5b72865e851fdfcf5987b68dff2b
ehansen-matc <ehansen@madisoncollege.edu>
2023-09-07 14:14:12 -0500

    Submitting Minestone 1 for Grading

---
+++ apache_log_analyzer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Author: Eric Hansen 
+Email: ehansen@madisoncollege.edu 
+Description: This code helps parse through apache log files to make them easier to read and know who is requsting info from a web server.
+"""
+
+intro = "This script will help you read apache log files"
+print(intro)
+
+userResponse = input("Would you like to continue? (y/n)")
+print(f"You said {userResponse}")
