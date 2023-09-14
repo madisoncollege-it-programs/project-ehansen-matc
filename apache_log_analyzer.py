b2aed3c99a57968f8226ceab3af7a9d14213e91c
ehansen-matc <ehansen@madisoncollege.edu>
2023-09-14 13:58:26 -0500

    submitting Milestone 2 for Grading

--- apache_log_analyzer.py
+++ apache_log_analyzer.py
@@ -6,7 +6,12 @@ Description: This code helps parse through apache log files to make them easier 
 """
 
 intro = "This script will help you read apache log files"
+apacheLog = '111.222.333.123 HOME - [01/Feb/1998:01:08:39 -0800] "GET /bannerad/ad.htm HTTP/1.0" 200 198 "http://www.referrer.com/bannerad/ba_intro.htm" "Mozilla/4.01 (Macintosh; I; PPC)"'
+apacheIP = apacheLog[0:15]
 print(intro)
 
 userResponse = input("Would you like to continue? (y/n)")
-print(f"You said {userResponse}")
+print(f"You responded: {userResponse}")
+print(f"Log Request from: {apacheIP:*^22}")             #formatted string to get just the ip surrounded by *
+logList = apacheLog.split(' ')                          #turns apacheLog into a list 
+print(f"Return code: {logList[8]}")                     #prints the 9th element in the list (0 being included)
