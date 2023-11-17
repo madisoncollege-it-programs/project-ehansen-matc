578fb556cdf49363c1bc4948810f45cb289fc6db
ehansen-matc <ehansen@madisoncollege.edu>
2023-11-17 01:35:03 -0600

    Submitting Milestone 10 for Grading

--- apache_log_analyzer.py
+++ apache_log_analyzer.py
@@ -6,9 +6,36 @@ Description: This code helps parse through apache log files to make them easier 
 """
 
 """ Milestone 9 """
-import sys                                                                  
-import subprocess                                                           
-import argparse #Makes argparse available to use 
+# import sys                                                                  
+# import subprocess                                                           
+# import argparse #Makes argparse available to use 
+
+# def IPAddressCount(apache_log_file_name):                                   
+#     completeProcess = subprocess.run(f"cat {apache_log_file_name} | cut -d ' ' -f1 | sort -n | uniq -c | sort -n | tail -n5" , stdout=subprocess.PIPE , shell=True)
+#     strOutput = completeProcess.stdout.decode()                             
+#     return strOutput                                                        
+
+# def main():                                                                  
+#     parser = argparse.ArgumentParser(description="A new parser for our script")                   #sets a variable called parser that I can use argparse's modules and functions with 
+#     parser.add_argument('-f' , '--filename' , dest='fileName' , metavar='[a log file]' , required=True , type=str , help='enter the filename')   #adds a argument that is required to enter before the script will run
+#     intro = "This script will help you read and analyze apache log files\n"
+#     print(intro)                                                             
+#     result = IPAddressCount(parser.parse_args().fileName)  #uses the variable that parser made called fileName in the IPAddressCount function                           
+#     print(result)                                                       
+#     outputFile = open("apache_analysis.txt" , "w")                      
+#     outputFile.write(result)                                  
+
+# if __name__ == '__main__':                                                  
+#     main()                                                                  
+
+""" Milestone 10 """
+import sys , subprocess , argparse , requests , bs4         #imported requests and bs4 to be used in the script 
+
+def IPLookup(IPAddress):
+    url = f"https://tools.keycdn.com/geo?host={IPAddress}"  #this sets a variable of the the URL with the supplied IP address
+    print(f"Going to '{url}' to find the location...")         #displays the url being used in the script 
+    response = requests.get(url)                            #this pulls all the HTML code from the website
+    return response
 
 def IPAddressCount(apache_log_file_name):                                   
     completeProcess = subprocess.run(f"cat {apache_log_file_name} | cut -d ' ' -f1 | sort -n | uniq -c | sort -n | tail -n5" , stdout=subprocess.PIPE , shell=True)
@@ -16,14 +43,22 @@ def IPAddressCount(apache_log_file_name):
     return strOutput                                                        
 
 def main():                                                                  
-    parser = argparse.ArgumentParser(description="A new parser for our script")                   #sets a variable called parser that I can use argparse's modules and functions with 
-    parser.add_argument('-f' , '--filename' , dest='fileName' , metavar='[a log file]' , required=True , type=str , help='enter the filename')   #adds a argument that is required to enter before the script will run
+    parser = argparse.ArgumentParser(description="A new parser for our script")                  
+    parser.add_argument('-f' , '--filename' , dest='fileName' , metavar='[a log file]' , required=True , type=str , help='enter the filename')
     intro = "This script will help you read and analyze apache log files\n"
     print(intro)                                                             
-    result = IPAddressCount(parser.parse_args().fileName)  #uses the variable that parser made called fileName in the IPAddressCount function                           
-    print(result)                                                       
+    result = IPAddressCount(parser.parse_args().fileName)
+    noNewLineList = result.split("\n")                                              #these 3 lines take the result from the linux command to get the highest occuring IP
+    noNewLineStr = ''.join(noNewLineList)
+    spaceList = noNewLineStr.split(" ")
+    highestOccurence = str(spaceList[-1])                                                #saves the highest occuring IP as a string 
+    print(f"[{highestOccurence}] is the highest occuring IP address in that log")   #prints the highest occuring IP 
+    response = IPLookup(highestOccurence)                                           #uses the new function with the highest occuring IP in the log file
+    #print(response.text[0:250])                                                    #the raw HTML with only the first 250 characters
+    myHTML = bs4.BeautifulSoup(response.text, features="html.parser")
+    print("The origin of the IP is in " + myHTML.find_all("dd", class_="col-8 text-monospace")[1].text)     #this is the supplied code that pulls the location of th IP from the website                                                                           
     outputFile = open("apache_analysis.txt" , "w")                      
     outputFile.write(result)                                  
 
 if __name__ == '__main__':                                                  
-    main()                                                                  
+    main()              
